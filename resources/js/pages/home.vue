6c1d059866ca0ceb03d1e9093d5eda9f67951ded
WesleyRXavier <git config --global user.email you@example.com>
2022-07-29 15:25:30 -0300

    first commit

---
+++ resources/js/pages/home.vue
@@ -0,0 +1,50 @@
+<template>
+  <admin-layout>
+    <v-banner class="mb-4">
+      <div class="d-flex flex-wrap justify-space-between">
+        <h5 class="text-h5 font-weight-bold">Home</h5>
+        <v-breadcrumbs :items="breadcrumbs" class="pa-0"></v-breadcrumbs>
+      </div>
+    </v-banner>
+    <v-card>
+     
+  <v-row justify="center">
+    <v-expansion-panels accordion>
+      <v-expansion-panel
+        v-for="(item,i) in 5"
+        :key="i"
+      >
+        <v-expansion-panel-header>Item</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </v-row>
+
+    </v-card>
+  </admin-layout>
+</template>
+
+<script>
+import AdminLayout from "../layouts/AdminLayout.vue";
+export default {
+  components: { AdminLayout },
+  data() {
+    return {
+      breadcrumbs: [
+        {
+          text: "App",
+          disabled: false,
+          href: "/home",
+        },
+        {
+          text: "Home",
+          disabled: true,
+          href: "/home",
+        },
+      ],
+    }
+  }
+};
+</script>
